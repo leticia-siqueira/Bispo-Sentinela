5b739c5be71ec17b838669480f0b7caf8e184724
Letícia Siqueira <leticiasiqueira1605@gmail.com>
2025-10-23 08:31:01 -0300

    Update Bispo_Sentinela
    Correção do While e enfeitamento do código

--- Questao_Bispo_Sentinela/Bispo_Sentinela.py
+++ Questao_Bispo_Sentinela/Bispo_Sentinela.py
@@ -1,5 +1,5 @@
-import os; os.system('cls')
-
+import os
+os.system('cls')
 
 '''tabuleiro:
      a      b      c      d      e      f      g      h
@@ -12,106 +12,69 @@ import os; os.system('cls')
 2  (0,1)  (1,1)  (2,1)  (3,1)  (4,1)  (5,1)  (6,1)  (7,1)
 1  (0,0)  (1,0)  (2,0)  (3,0)  (4,0)  (5,0)  (6,0)  (7,0)
 
-
-
-
 Posição das diagonais:
-
 
 cima e direita = (x + 1, y + 1)
 cima e esquerda = (x - 1, y + 1)
 baixo e direita = (x + 1, y - 1)
-baixo e esquerda = (x - 1, y -1)
-
+baixo e esquerda = (x - 1, y -1)]
 
 '''
 
 # 1. Converte de Alfanumérico para Coordenada (string -> (x, y))
-# !!!Lembrar que x = coluna e y = linha
+# !!!Lembrar que x = coluna e y = linha 
 def alf_to_coord(s):
-    # converte a coluna
     x = ord(s[0]) - ord('a')
-   
-    # converte a linha
     y = int(s[1]) - 1
-   
     return x, y
 
-
-# 2. Coordenada para Alfanumérico (x, y) -> string
 def coord_to_alf(x, y):
-    # chr faz o oposto de ord
     coluna = chr(x + ord('a'))
     linha = str(y + 1)
-   
     return coluna + linha
 
-
+#Lista criada para fazer a verificação de itens repetidos
 lista_posições_todos = []
 
-
-bispo = input("Digite a posição do bispo: ")
+bispo = input("Digite a posição do bispo: ").lower()
 lista_posições_todos.append(bispo)
 bispo = alf_to_coord(bispo)
 
-
-peças = set() #armazena só as coordenadas das peças inimigas, sem incluir o bispo
-
-
-
+peças = set() 
 
 qtd_peças_inimigas = int(input("Digite a quantidade de peças inimigas: "))
 
-
-if qtd_peças_inimigas < 0 or qtd_peças_inimigas > 63:
-    while True:
-        print("O número tem que ser entre 0 e 63")
-        qtd_peças_inimigas = int(input("Digite uma quantidade válida: "))
-        if qtd_peças_inimigas >= 0 and qtd_peças_inimigas <=63:
-            break
-        else:
-            continue
-
-
-
-
-
+while qtd_peças_inimigas < 0 or qtd_peças_inimigas > 63:
+    print("O número tem que ser entre 0 e 63")
+    qtd_peças_inimigas = int(input("Digite uma quantidade válida: "))
 
 for i in range(qtd_peças_inimigas):
-    posição_inimigo = input(f"Digite a posição do inimigo {i} em notação Algébrica: ").lower()
-    while True:
-        if posição_inimigo in lista_posições_todos:
-            print("Já tem uma peça nessa casa")
-            posição_inimigo = input("Digite uma posição diferente: ")
-        else:
-            lista_posições_todos.append(posição_inimigo)
-            peças.add(alf_to_coord(posição_inimigo))
-            break
-
+    posição_inimigo = input(f"Digite a posição do inimigo {i+1} em notação Algébrica: ").lower()
+    
+    while posição_inimigo in lista_posições_todos: #verificação de itens repetidos, e se não tá ocupando aa posição do bispo
+        print("Já tem uma peça nessa casa!")
+        posição_inimigo = input("Digite uma posição diferente: ").lower()
+    
+    lista_posições_todos.append(posição_inimigo)
+    peças.add(alf_to_coord(posição_inimigo))
 
 peças_capturaveis = set()
 
-
-def diagonais (dx, dy,bispo,peças):
+#Função pra percorrer todas as diagonais
+def diagonais(dx, dy, bispo, peças):
     #cima e direita = (x + 1, y + 1)
     #cima e esquerda = (x - 1, y + 1)
     #baixo e direita = (x + 1, y - 1)
     #baixo e esquerda = (x - 1, y -1)
-    x,y = bispo #(2,3)
-
-
-    #cálculo para qualquer diagonal:
-    while x <= 7 and x >=0 or y <= 7 and y >= 0: #enquanto ele está no tabuleiro
-        x += dx #dx = 1
-        y += dy #dy = 1
-       
-       
-        if (x,y) in peças:
-            peças_capturaveis.add(coord_to_alf(x,y))  
-            break
-        else:
-            continue
-
+    
+    x, y = bispo
+    while 0 <= x <= 7 and 0 <= y <= 7: #enquanto ele não saiu do tabuleiro, contudo ele ainda pode sair depois da primeira condição
+        x += dx
+        y += dy
+        if 0 <= x <= 7 and 0 <= y <= 7:
+            if (x, y) in peças:#significa que tem uma peça naquela diagonal e o bispo pode comer ele
+                peças_capturaveis.add(coord_to_alf(x, y))
+                break
 
 for i in range(4):
     if i == 0: #pra cima e pra direita
@@ -126,14 +89,14 @@ for i in range(4):
         dx = 1
         dy = -1
         diagonais(dx,dy,bispo,peças)
-    elif i == 3: #pra cima e pra esquerda
+    elif i == 3: #pra baixo e pra esquerda
         dx = -1
         dy = -1
         diagonais(dx,dy,bispo,peças)
 
 
 if len(peças_capturaveis) == 0:
-    print(0)
+    print("\nNenhuma peça pode ser capturada(0);-;")
 else:
-    print(len(peças_capturaveis))
+    print(f"\n{len(peças_capturaveis)} peça(s) podem ser capturadas:")
     print(sorted(peças_capturaveis))
